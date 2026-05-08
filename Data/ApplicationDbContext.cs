553d893b3de25dc623347a0f6eb5dee77d7b841b
cance1a <147398010+cance1a@users.noreply.github.com>
2026-05-08 10:44:03 +0100

    Add files via upload

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EmployeesManagement.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmployeesManagement.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext: IdentityDbContext
     {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        { 
+        }
+        
+        public DbSet<Employee> Employees { get; set; }
     }
 }
---
+++ Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace EmployeesManagement.Models
+{
+    public class Employee: UserActivity
+    {
+        public int id { get; set; }
+
+        public string EmpNo { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public string FullName => $"{FirstName} {MiddleName} {LastName}";
+
+        public int PhoneNumber { get; set; }
+
+        public string EmailAdress { get; set; }
+
+        public string Country { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Adress { get; set; }
+
+        public string Department { get; set; }
+
+        public string Designation { get; set; }
+    }
+}
