1663c7fead36e9a1ebc6e3261462cbee601f780c
cance1a <147398010+cance1a@users.noreply.github.com>
2026-05-15 10:16:52 +0000

    add da view details.cshtml em instruments

--- obj/Debug/net10.0/EmployeesManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/EmployeesManagement.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeesManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+725ab2dff122d93f863f2685b3694dbbe5bb5c2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90def115e2a371c1e1216770494c7152cc9c627a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeesManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeesManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
