855acf01b8b6fee20858cec6103adc290364d889
cance1a <147398010+cance1a@users.noreply.github.com>
2026-05-15 09:12:11 +0000

    Insert model Intrument.cs

--- obj/Debug/net10.0/EmployeesManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/EmployeesManagement.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeesManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553d893b3de25dc623347a0f6eb5dee77d7b841b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dc4214ce63ea4ef856ce52bae602bafaa92390f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeesManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeesManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
