dff7c2369c26ec5ae1df7fe405172b72201d3727
cance1a <147398010+cance1a@users.noreply.github.com>
2026-05-15 10:47:28 +0000

    ultimo commit para ter a certeza que esta tudo a correr bem

--- obj/Debug/net10.0/EmployeesManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/EmployeesManagement.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeesManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b73203a3cb48b2791f2537c8b4444db5119da79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6e268bd8733b5a4974accd0e64ca0ce9212037")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeesManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeesManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
